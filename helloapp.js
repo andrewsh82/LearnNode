f556604b58d2b80cf955c360ff388162460bf71e
Andrew <andrewshved@gmail.com>
2015-10-18 18:48:22 -0400

    adding uploads page

--- helloapp.js
+++ helloapp.js
@@ -7,6 +7,10 @@ var handler = function(req,res){
 	} else if (req.url==="/marketing") {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 		res.end('Buy Crap\n');
+		
+	} else if (req.url==="/uploads"){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('<html><head><title>UPLOADZ!</title><body><form action="/uploads" method="POST" enctype="multipart/form-data"><input type="file" name="the_file"><input type="submit" value="Upload">');
 	} else {
 		res.writeHead(404, {'Content-Type': 'text/plain'});
 		res.end('Page not Found. \n');
