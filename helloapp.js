03173374c5f906079fe4485053f31f5b87ae819a
Andrew <andrewshved@gmail.com>
2015-10-18 18:37:07 -0400

    routing marketing and 404

--- helloapp.js
+++ helloapp.js
@@ -1,7 +1,16 @@
 var http = require('http');
 var handler = function(req,res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');	
+	console.log(req.url);
+	if (req.url==="/") {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end('Hello World\n');
+	} else if (req.url==="/marketing") {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end('Buy Crap\n');
+	} else {
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.end('Page not Found. \n');
+	}	
 }
 
 var server=http.createServer(handler);
